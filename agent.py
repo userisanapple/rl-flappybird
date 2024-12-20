a0bf64511c29315f8a9e4e3cd19a4fc3e5495749
userisanapple <coolapple14@gmail.com>
2024-12-20 14:10:15 -0800

    update state representation, reintroduce reward shaping

--- agent.py
+++ agent.py
@@ -19,17 +19,18 @@ class FlappyBirdAgent:
         self.epsilon = start_epsilon
         self.epsilon_decay = epsilon_decay
         self.end_epsilon = end_epsilon
-        self.h_bins = np.linspace(-1, 1, num=3) 
-        self.v_bins = np.linspace(-1, 1, num=15) 
-        self.r_bins = np.linspace(-1, 1, num=10) 
+        self.h_bins = np.linspace(-1, 1, num=30) 
+        self.v_bins = np.linspace(-1, 1, num=50) 
         self.rng = np.random.default_rng(seed=seed)
-        self.q_table = np.full((self.v_bins.size, self.h_bins.size, self.v_bins.size, self.v_bins.size, self.r_bins.size, env.action_space.n), fill_value=10.0)
+        q_size = (self.v_bins.size, self.h_bins.size, self.v_bins.size, env.action_space.n)
+        # self.q_table = np.full(q_size, fill_value=10.0)
+        self.q_table = np.zeros(q_size)
+        # self.q_table = rng.uniform(low=0, high=0.1, shape=q_size)
 
     def obs_to_state(self, obs: gym.spaces.Space):
         # get vertical distance between two pipes
         last_pipe_v = obs[2]
         next_pipe_v = obs[5]
-        pipe_v_distance = last_pipe_v - next_pipe_v
 
         # get horizontal and vertical distance to next pipe
         last_pipe_h = obs[0]
@@ -38,28 +39,43 @@ class FlappyBirdAgent:
 
         next_pipe_distance = next_pipe_h
         player_pipe_v_distance = player_v - next_pipe_v
-        if last_pipe_h >= 0:
+        if last_pipe_h >= -0.15:
             next_pipe_distance = last_pipe_h
             player_pipe_v_distance = player_v - last_pipe_v
 
         player_velocity = obs[10]
-        player_rot = obs[11]
 
         b1 = np.digitize(player_pipe_v_distance, self.v_bins) - 1
         b2 = np.digitize(next_pipe_distance, self.h_bins) - 1
-        b3 = np.digitize(pipe_v_distance, self.v_bins) - 1
-        b4 = np.digitize(player_velocity, self.v_bins) - 1
-        b5 = np.digitize(player_rot, self.r_bins) - 1
+        b3 = np.digitize(player_velocity, self.v_bins) - 1
 
-        return (b1, b2, b3, b4, b5)
+        return (b1, b2, b3)
     
     def get_action(self, obs: gym.spaces.Space):
         if self.rng.random() < self.epsilon:
             return self.env.action_space.sample()
         q_state = self.q_table[self.obs_to_state(obs)]
         return np.argmax(q_state)
+    
+    def shape_reward(self, obs: gym.spaces.Space, reward: float):
+        shaped_reward = reward
+        if np.isclose(reward, 1.0):
+            shaped_reward = 25.0
+        elif np.isclose(reward, -1.0):
+            shaped_reward = -10.0
+
+        player_v = obs[9]
+        next_pipe_v = obs[5]
+        last_pipe_h = obs[0]
+        if last_pipe_h >= -0.15:
+            next_pipe_v = obs[2]
+        shaped_reward -= abs((player_v-next_pipe_v) * 2)
+
+        return shaped_reward
 
     def update(self, old_state: tuple, action: int, reward: float, terminated: bool, obs: gym.spaces.Space):
+        reward = self.shape_reward(obs, reward)
+
         q_sa = self.q_table[old_state][action]
         next_action = (not terminated) * np.max(self.q_table[self.obs_to_state(obs)])
         q_sa_next = self.df * next_action
