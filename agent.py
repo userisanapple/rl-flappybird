06e52a66f0a9d7496f67f3b669378a4ae334d1b1
userisanapple <coolapple14@gmail.com>
2024-12-20 16:35:44 -0800

    implement resuming from episode, lr decay, and print more info on save interval

--- agent.py
+++ agent.py
@@ -1,31 +1,44 @@
 import gymnasium as gym
 import numpy as np
+from os import PathLike
 
 class FlappyBirdAgent:
     def __init__(
         self,
         *,
         env: gym.Env,
-        start_epsilon: float,
-        epsilon_decay: float,
-        end_epsilon: float,
-        learning_rate: float = 0.01,
+        start_lr: float = 0.1,
+        lr_decay: float = 0.0005,
+        end_lr: float = 0.01,
+        start_epsilon: float = 1.0,
+        epsilon_decay: float = 0.005,
+        end_epsilon: float = 0.1,
         discount_factor: float = 0.95,
-        seed: int = 1234
+        seed: int = 1234,
+        start_episode: PathLike = None,
     ):
         self.env = env
-        self.lr = learning_rate
         self.df = discount_factor
-        self.epsilon = start_epsilon
+        if start_episode:
+            self.lr = end_lr
+            self.epsilon = end_epsilon
+        else:
+            self.lr = start_lr
+            self.epsilon = start_epsilon
+        self.lr_decay = lr_decay
+        self.end_lr = end_lr
         self.epsilon_decay = epsilon_decay
         self.end_epsilon = end_epsilon
         self.h_bins = np.linspace(-1, 1, num=30) 
         self.v_bins = np.linspace(-1, 1, num=50) 
         self.rng = np.random.default_rng(seed=seed)
         q_size = (self.v_bins.size, self.h_bins.size, self.v_bins.size, env.action_space.n)
-        # self.q_table = np.full(q_size, fill_value=10.0)
-        self.q_table = np.zeros(q_size)
-        # self.q_table = rng.uniform(low=0, high=0.1, shape=q_size)
+        if start_episode:
+            self.q_table = np.load(start_episode)
+        else:
+            # self.q_table = np.full(q_size, fill_value=10.0)
+            self.q_table = np.zeros(q_size)
+            # self.q_table = rng.uniform(low=0, high=0.1, shape=q_size)
 
     def obs_to_state(self, obs: gym.spaces.Space):
         # get vertical distance between two pipes
@@ -81,5 +94,6 @@ class FlappyBirdAgent:
         q_sa_next = self.df * next_action
         self.q_table[old_state][action] = q_sa + (self.lr * (reward + q_sa_next - q_sa))
     
-    def decay_epsilon(self):
+    def decay(self):
+        self.lr = max(self.end_lr, self.lr - self.lr_decay)
         self.epsilon = max(self.end_epsilon, self.epsilon - self.epsilon_decay)
--- train.py
+++ train.py
@@ -9,14 +9,21 @@ import os
 
 
 if __name__ == '__main__':
-    n_episodes = 500_000
-    save_interval = 5000
+    n_episodes = 100_000
+    save_interval = 10000
 
     env = gym.make("FlappyBird-v0", render_mode="rgb_array", use_lidar=False)
     env = RecordVideo(env, video_folder="episodes", name_prefix="training", episode_trigger=lambda x: x % save_interval == 0)
     env = RecordEpisodeStatistics(env)
 
-    agent = FlappyBirdAgent(env=env, start_epsilon=1.5, epsilon_decay=1.0/(n_episodes), end_epsilon=0.1)
+    agent = FlappyBirdAgent(
+        env=env,
+        start_lr=0.1,
+        lr_decay=1.0/(n_episodes/2),
+        start_epsilon=1.5,
+        epsilon_decay=1.0/(n_episodes/2),
+        end_epsilon=0.1,
+    )
 
     interval_total_reward = 0
     interval_total_len = 0
@@ -30,23 +37,24 @@ if __name__ == '__main__':
 
             # Processing:
             obs, reward, terminated, _, info = env.step(action)
-            if 'episode' in info:
-                interval_total_reward += info['episode']['r']
-                interval_total_len += info['episode']['l']
 
             agent.update(state, action, reward, terminated, obs)
             
             # Checking if the player is still alive
             if terminated:
+                if 'episode' in info:
+                    interval_total_reward += info['episode']['r']
+                    interval_total_len += info['episode']['l']
                 break
-            agent.decay_epsilon()
+            agent.decay()
 
         if not episode % save_interval:
             if not os.path.exists('episodes'):
                 os.mkdir('episodes')
             print(f'avg score: {interval_total_reward/save_interval}', end='\t')
             if 'episode' in info:
-                print(f'avg len: {interval_total_len/save_interval}',end='\t')
+                print(f'avg len: {interval_total_len/save_interval}', end='\t')
+            print(f'lr: {agent.lr}', end='\t')
             print(f'epsilon: {agent.epsilon}')
             interval_total_reward = 0
             episode_path = os.path.join('episodes', f'{episode}-qtable.npy')
