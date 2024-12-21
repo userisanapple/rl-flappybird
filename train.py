f97cd04e9c36622b35c0a57287ca4224bddb975e
userisanapple <coolapple14@gmail.com>
2024-12-20 18:26:47 -0800

    output plots on save interval

--- train.py
+++ train.py
@@ -8,6 +8,26 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 
+def plot_data(n_episodes: int, ep_min_scores: list, ep_cumulative_scores: list, ep_max_scores: list, ep_lengths: list) -> tuple:
+    plt.close()
+    fig, ax = plt.subplots(1, 2, figsize=(15,5), sharex=True)
+    linspace_x = np.linspace(0, n_episodes, num=n_episodes, dtype=int)
+    scores_ax = ax[0]
+    scores_ax.plot(linspace_x, ep_min_scores, label='Min episode reward')
+    scores_ax.plot(linspace_x, ep_cumulative_scores, label='Cumulative episode reward')
+    scores_ax.plot(linspace_x, ep_max_scores, label='Max episode reward')
+    scores_ax.set_ylabel('Reward')
+    scores_ax.legend()
+
+    length_ax = ax[1]
+    length_ax.plot(linspace_x, ep_lengths, label='Episode length')
+    length_ax.set_ylabel('Length')
+    length_ax.legend()
+    
+    fig.supxlabel('Episode #')
+    plt.tight_layout()
+    return (fig, ax)
+
 if __name__ == '__main__':
     n_episodes = 100_000
     save_interval = 10000
@@ -65,9 +85,7 @@ if __name__ == '__main__':
                 break
             agent.decay()
 
-        if not episode % save_interval:
-            if not os.path.exists('episodes'):
-                os.mkdir('episodes')
+        if not episode % save_interval and episode > 0:
             print(f'avg score: {interval_total_reward/save_interval}', end='\t')
             if 'episode' in info:
                 print(f'avg len: {interval_total_len/save_interval}', end='\t')
@@ -75,25 +93,20 @@ if __name__ == '__main__':
             print(f'epsilon: {agent.epsilon}', end='\t')
             interval_total_reward = 0
             interval_total_len = 0
+
+            # make episode dir
+            if not os.path.exists('episodes'):
+                os.mkdir('episodes')
+
+            # save qtable
             episode_path = os.path.join('episodes', f'{episode}-qtable.npy')
             np.save(episode_path, agent.q_table)
+
+            # save plot
+            fig, ax = plot_data(episode+1, ep_min_scores, ep_cumulative_scores, ep_max_scores, ep_lengths)
+            plt.savefig(os.path.join('episodes', f'{episode}-plots.png'))
         
     env.close()
 
-    fig, ax = plt.subplots(1, 2, figsize=(15,5), sharex=True)
-    linspace_x = np.linspace(0, n_episodes, num=100)
-    scores_ax = ax[0]
-    scores_ax.plot(linspace_x, ep_min_scores, label='Min episode score')
-    scores_ax.plot(linspace_x, ep_cumulative_scores, label='Cumulative episode score')
-    scores_ax.plot(linspace_x, ep_max_scores, label='Max episode score')
-    scores_ax.set_ylabel('Score')
-    scores_ax.legend()
-
-    length_ax = ax[1]
-    length_ax.plot(linspace_x, ep_lengths, label='Episode length')
-    length_ax.set_ylabel('Length')
-    length_ax.legend()
-    
-    fig.supxlabel('Episode #')
-    plt.tight_layout()
+    fig, ax = plot_data(n_episodes, ep_min_scores, ep_cumulative_scores, ep_max_scores, ep_lengths)
     plt.show()
